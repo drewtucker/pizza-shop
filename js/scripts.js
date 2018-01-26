8f27ff342abcc904fb9851426374505288a960d6
Drew Tucker <dtuck43@gmail.com>
2018-01-26 12:32:24 -0800

    Add forEach loop to Price function that adds 1 to finalPrice for every topping in the Pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 function Pizza(size, toppings){
 	this.size = size;
-	this.toppings = toppings;}
+	this.toppings = toppings;
+  };
+
+var finalPrice = 0;
+var Test = new Pizza("Large", ["Olives", "Cheese", "Shit", "Stuff"]);
+console.log(Test.toppings);
+console.log(Test.toppings.length);
+
+  Pizza.prototype.Price = function(){
+  this.toppings.forEach(function(topping){
+    finalPrice += 1;
+  });
+  };
+
+Test.Price();
+console.log(finalPrice);
